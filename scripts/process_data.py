f775839812044b925d237660f8ec39c8ee0de975
Violet Hart <vihart.rl@gmail.com>
2025-05-15 20:27:17 -0400

    add game id for data grouping

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,24 +1,32 @@
 import os
 import sys
+import uuid
 from pathlib import Path
 from rlgym_tools.rocket_league.replays.parsed_replay import ParsedReplay
 import rocketxg as rxg
 import pandas as pd
 from rocketxg.hit_analysis import get_shooter_analysis, get_opponent_analysis, get_ball_analysis
 
-def run(replay_dir: str):
+def run(replay_dir: str, out_dir: str):
     for path in Path(replay_dir).rglob('*.replay'):
-        out_path = Path("data/test.parquet")
-        os.makedirs(out_path.parent, exist_ok=True)
+        if path.is_dir():
+            continue
+        print(f"Path: {path}")
+        os.makedirs(out_dir, exist_ok=True)
+        out_path = Path(out_dir) / "data.parquet"
         replay = ParsedReplay.load(path)
         hits = rxg.generate_hits_table(replay)
         players = list(hits.keys())
+        game_id = uuid.uuid4()
         dataset = []
         for player in players:
             player.generate_possessions()
             player.generate_shots(replay, time_s=3)
             for shot, data in player.shots.items():
-                game_state = {"is_goal": shot.is_goal}
+                game_state = {
+                    "game_id": game_id.hex,
+                    "is_goal": shot.is_goal
+                }
                 ball_data = get_ball_analysis(shot, replay)
                 game_state.update(ball_data)
                 opponent_data = get_opponent_analysis(shot, players, replay)
@@ -42,4 +50,5 @@ def run(replay_dir: str):
     
 if __name__ == "__main__":
     replay_dir = sys.argv[1]
-    run(replay_dir)
+    out_dir = sys.argv[2]
+    run(replay_dir, out_dir)
